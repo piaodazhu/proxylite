4b688f58cbaed56dc515e37733c90f35b08a13be
piaodazhu <2283079466@qq.com>
2023-10-08 16:29:33 +0800

    fix tunnel close bug & add a cli example

--- example/server/main.go
+++ example/server/main.go
@@ -3,6 +3,6 @@ import "github.com/piaodazhu/proxylite"
 
 func main() {
 	server := proxylite.NewProxyLiteServer()
-	server.AddPort(9930, 9932)
+	server.AddPort(9940, 9948)
 	panic(server.Run(":9933"))
 }
